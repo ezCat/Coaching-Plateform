0320f706ac1cd9c8f543aa5932b68851714ffab7
Dennis <dennis_wittmer@yahoo.fr>
2016-12-22 22:59:09 +0100

    WS-3 - Make migrations files

---
+++ database/migrations/2016_12_22_215725_create_muscles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMusclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('muscles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('muscles');
+    }
+}
