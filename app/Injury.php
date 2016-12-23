646032fe448e8db091309db58bdb59fac2f37cb6
ezCat <dennis_wittmer@yahoo.fr>
2016-12-23 15:58:34 +0100

    WS-3 - Create HTTP Controllers

--- app/Injury.php
+++ app/Injury.php
@@ -4,13 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Muscle extends Model
+class Injury extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'date_injury', 'date_recover_injury', 'player_id', 'team_id', 'pathology_id'];
 
-    protected $table = "muscles";
+    protected $table = "injuries";
 
-    public function injuries() {
-        return $this->belongsToMany('App\Injury');
+    public function pathologies() {
+        return $this->hasOne('App\Pathology');
     }
 }
