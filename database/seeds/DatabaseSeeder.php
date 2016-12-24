cd7bdb2f8769d223c1d82d4955455f1793bf5101
Dennis <dennis_wittmer@yahoo.fr>
2016-12-24 13:54:49 +0100

    WS-3 - Add Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -61,11 +61,13 @@ class DatabaseSeeder extends Seeder
 			    'team_id' => $index,
 			    'player_id' => $index,
 			]);
-	}
-	DB::table('documents')->insert([
-		'name' => $faker->word,
-		'link' => 'https://drive.google.com/open?id=0Bwg_0Xqlmx88dk1WYVJzY1Rtb3c',
-		'team_id' => 1
-	]);
+		}
+		DB::table('documents')->insert([
+			'name' => $faker->word,
+			'link' => 'https://drive.google.com/open?id=0Bwg_0Xqlmx88dk1WYVJzY1Rtb3c',
+			'team_id' => 1
+		]);
+
+		$this->call(InjuriesSeeder::class);
     }
 }
