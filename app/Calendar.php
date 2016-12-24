b8af04f96394b91d5f11dc54618cdf15e96b714c
ezCat <dennis_wittmer@yahoo.fr>
2016-12-24 13:16:18 +0100

    WS-4 - Change Model

--- app/Calendar.php
+++ app/Calendar.php
@@ -6,11 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Calendar extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'title', 'content', 'begun_at', 'finished_at', 'location', 'calendar_type_id', 'team_id'];
 
     protected $table = "calendars";
 
     public function teams() {
         return $this->belongsToMany('App\Team');
+    }
+
+    public function calendarTypes() {
+        return $this->hasOne('App\CalendarTypes');
     }
 }
