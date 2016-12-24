831dd429eb0de0db0361a445fbc80bf04d7312f3
ezCat <dennis_wittmer@yahoo.fr>
2016-12-24 13:23:30 +0100

    WS-11 - Add Model & migration

---
+++ app/Schedule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    protected $fillable = ['date_match', 'club_id', 'team_id', 'place_id'];
+
+    protected $table = "schedules";
+
+    public function teams() {
+        return $this->belongsToMany('App\Team');
+    }
+
+    public function places() {
+        return $this->hasOne('App\Place');
+    }
+}
---
+++ database/migrations/2016_12_24_121905_create_schedules_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('date_match');
+            $table->increments('club_id')->unsigned();
+            $table->increments('place_id')->unsigned();
+            $table->increments('team_id')->unsigned();
+        });
+
+        Schema::table('schedules', function (Blueprint $table) {
+            $table->foreign('club_id')->references('id')->on('clubs');
+            $table->foreign('place_id')->references('id')->on('places');
+            $table->foreign('team_id')->references('id')->on('teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
