165003f2eec0d655b82e1c5af879958694983497
ezCat <dennis_wittmer@yahoo.fr>
2016-12-27 14:38:47 +0100

    WS-14 - Correction

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,7 @@
 require('./bootstrap');
 
 Vue.component('teams', require('./components/Team/Teams.vue'));
+Vue.component('list-teams', require('./components/Team/TeamsList.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/assets/js/components/Team/TeamsList.vue
+++ resources/assets/js/components/Team/TeamsList.vue
@@ -26,5 +26,6 @@
                     this.teams = response.data
                 });
             }
+        }
     }
 </script>
