992dcf42b5b84bae2740038106c95ef6fc5cc197
ezCat <dennis_wittmer@yahoo.fr>
2016-12-28 17:13:45 +0100

    WS-14 - Finish team vue
    Foundation import

--- resources/assets/js/components/Layout/Navbar.vue
+++ resources/assets/js/components/Layout/Navbar.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="top-bar">
+<div>
+    <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="large">
+        <button class="menu-icon" type="button" data-toggle="example-menu"></button>
+        <div class="title-bar-title">UCOACH</div>
+    </div>
+
+    <div class="top-bar" id="example-menu">
         <div class="top-bar-left">
-            <ul class="dropdown menu" data-dropdown-menu>
+            <ul class="menu">
                 <li class="menu-text">UCOACH</li>
             </ul>
         </div>
@@ -10,12 +16,13 @@
                 <li><a href="#"><i class="fa fa-home"></i> HOME</a></li>
                 <li><a href="#"><i class="fa fa-bell"></i> REMINDER</a></li>
                 <li><a href="#"><i class="fa fa-calendar"></i> CALENDAR</a></li>
-                <li><a href="#"><i class="fa fa-file-o"></i> DOCUMENTS</a></li>
+                <li><a href="#"><i class="fa fa-folder"></i> DOCUMENTS</a></li>
                 <li><a href="#"><i class="fa fa-calendar-o"></i> SCHEDULE</a></li>
                 <li><a href="#"><i class="fa fa-medkit"></i> INJURIES</a></li>
             </ul>
         </div>
     </div>
+</div>
 </template>
 <style>
 </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,18 @@
     <meta name="_token" content="{{ csrf_token() }}"/>
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Laravel 5.3 with Vuejs</title>
+    <title>One pager</title>
 </head>
 <body>
 <div id="app" class="container">
     <navbar></navbar>
     <teams></teams>
 </div>
+<script src="/js/jquery.js"></script>
+<script src="/js/foundation.js"></script>
 <script src="/js/app.js"></script>
+<script type="application/javascript">
+    $(document).foundation();
+</script>
 </body>
 </html>
