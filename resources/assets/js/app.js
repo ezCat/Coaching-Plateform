fd4a9bc49f75a9c92ce6f3e32172955b6e01b924
ezCat <dennis_wittmer@yahoo.fr>
2016-12-29 13:36:35 +0100

    WS-Master - Delete app.js components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,13 +2,9 @@ require('./bootstrap');
 
 Vue.component('navbar', require('./components/Layout/Navbar.vue'));
 Vue.component('teams', require('./components/Team/Teams.vue'));
-Vue.component('list-teams', require('./components/Team/TeamsList.vue'));
 Vue.component('documents', require('./components/Document/Documents.vue'));
-Vue.component('list-documents', require('./components/Document/DocumentsList.vue'));
 Vue.component('schedules', require('./components/Schedule/Schedules.vue'));
-Vue.component('list-schedules', require('./components/Schedule/SchedulesList.vue'));
 Vue.component('reminders', require('./components/Reminder/Reminders.vue'));
-Vue.component('list-reminders', require('./components/Reminder/RemindersList.vue'));
 
 const app = new Vue({
     el: '#app'
