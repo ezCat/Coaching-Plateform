37cc02a7f4bc61d1b229a15fe24b3b25139c37e1
ezCat <dennis_wittmer@yahoo.fr>
2016-12-29 14:07:32 +0100

    WS-17 - Club Core

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,6 +6,7 @@ Vue.component('documents', require('./components/Document/Documents.vue'));
 Vue.component('schedules', require('./components/Schedule/Schedules.vue'));
 Vue.component('reminders', require('./components/Reminder/Reminders.vue'));
 Vue.component('injuries', require('./components/Injury/Injuries.vue'));
+Vue.component('clubs', require('./components/Club/Clubs.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
 <body>
 <div id="app" class="container">
     <navbar></navbar>
+    <clubs></clubs>
     <injuries></injuries>
     <reminders></reminders>
     <schedules></schedules>
